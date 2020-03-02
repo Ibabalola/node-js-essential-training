fa9beee16a353718c6d8ddd0ade63d7c657cf249
Isaac Babalola <isaac.babalola@capgemini.com>
2020-03-02 08:48:16 +0000

    Understanding the global process object and adding README.md

--- global.js
+++ global.js
@@ -4,4 +4,9 @@ const path = require("path"); // 'path' is module that ship with nodeJS
 console.log(__dirname);
 console.log(__filename);
 
+// to pluck the file name from the full path 
 console.log(`The file name is ${path.basename(__filename)}`);
+
+// Every node JS file we create is referred to as a module, it contains it's own code
+// modules are loaded using the require function that is available to us
+// on the global object
