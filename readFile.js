9fe61fe9584c137b0b6f0d95e084f2a010a6aee2
Isaac Babalola <isaac.babalola@capgemini.com>
2020-04-28 21:14:41 +0100

    adding module to read files

--- readFile.js
+++ readFile.js
@@ -12,6 +12,8 @@ const fs = require("fs");
 
 // Binary - no encoding provided
 // content of the file are read into a buffer
+// Buffer is a region of a physical memory storage used to 
+// temporarily store data while it is bening moved from one place to another
 fs.readFile("./assets/isaac.png", (err, img) => {
 
     if (err) {
