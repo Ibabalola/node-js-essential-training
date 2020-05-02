f0d2e913cbcacefbc7485c3cf22e7a06770a10cf
Isaac Babalola <isaac.babalola@capgemini.com>
2020-05-02 19:39:42 +0100

    adding module to execute child processes

---
+++ exec.js
@@ -0,0 +1,21 @@
+// The child process module allows the process
+// to execute external process within your environment
+const cp = require("child_process");
+
+// the exec is for synchronous functions
+// cp.exec("open https://www.isbonline.co.uk/");
+
+// open another terminal
+// cp.exec("open -a Terminal .");
+
+// cp.exec("ls", (err, data, stderr) => {
+//     if (err) {
+//         throw err;
+//     }
+
+//     console.log(data);
+// });
+
+cp.exec("node readStream", (err, data, stderr) => {
+    console.log(data);
+});
